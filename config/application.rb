5f2d03f455ec51a40b70eaa125d9fa55599d6228
Lucasdfg07 <lucassiqueirafernandes07@gmail.com>
2021-11-29 12:47:06 -0300

    Allow AWS host to access application

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module WebService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.hosts << "amanda-139144429.us-east-1.elb.amazonaws.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
